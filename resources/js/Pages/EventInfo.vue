f03f0158f5c679b658fc2438f2b212399c17be44
mcufi <mcufi@cendrassos.net>
2025-01-16 19:37:43 +0100

    EventInfo View Done

---
+++ resources/js/Pages/EventInfo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex justify-center font-bold">
+    <h1 class="text-5xl font-bold">Esdeveniment1</h1>
+    </div>
+
+    <div class="flex flex-row h-screen w-full justify-center items-center">
+    <div>
+      <img src="/images/logo.png" alt="Event_Image" width="500" height="500">
+    </div>
+
+    <div class="ml-20">
+      <p class="text-3xl font-bold">Titol Esdeveniment</p>
+      <p>Descripció de l'esdeveniment</p>
+      <p class="text-2xl font-bold">Localització:</p>
+      <p>Mapa</p>
+      <p class="text-2xl font-bold">Data i Hora:</p>
+      <p>dd/mm/yyyy - hh:mm</p>
+    </div>
+</div>
+</template>
