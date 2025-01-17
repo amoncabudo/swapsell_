d5fe7a54f1e7079242102b484aa9001c372c0865
cdecaprio <cdecaprio@cendrassos.net>
2025-01-17 12:03:50 +0100

    register funciona

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,19 +4,21 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
-    >
+    <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
+        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
+
         <div>
             <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+                <img class="w-24 h-24 mx-auto rounded-full shadow-2xl p-2 bg-white width=" 
+                 src="/images/logo.png"
+                 alt="SwapSell Logo"
+                 style="width: 170px; height: 150px;">
             </Link>
         </div>
 
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800"
-        >
+        
             <slot />
         </div>
-    </div>
+        </div>
+    
 </template>
