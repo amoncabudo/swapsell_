d2e073b1768144215a42c840b21d111667c10e6c
Jho077 <jespinoza@cendrassos.net>
2025-01-28 18:30:46 +0100

    #126 Se ven las imagenes ya

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,9 +29,18 @@ class ProductController extends Controller
         $product->latitude = $latitude;
         $product->status = $status;
         $product->user_id = Auth::id();
-        $product->category_id = 1;
-        $product->image = 'default.jpg';
-
+        $product->category_id = $request->get('category_id') ?? 1;
+        //Image
+        
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->storeAs('public', $request->file('image')->getClientOriginalName());
+            $product->image = $request->file('image')->getClientOriginalName();
+        } else {
+            $product->image = 'default.jpg';
+        }   
+                
+        
+        
         $product->save();
         return redirect()->route('Products')->with('success', 'Producte publicat correctament');
     }
