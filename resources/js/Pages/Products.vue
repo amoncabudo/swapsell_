6bd6d33bc9b658ede8fc1436155b36eeeebb2bd7
mcufi <mcufi@cendrassos.net>
2025-02-10 15:34:12 +0100

    fix multiple errors

--- resources/js/Pages/Products.vue
+++ resources/js/Pages/Products.vue
@@ -204,6 +204,7 @@ const filteredProducts = computed(() => {
                       d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   </svg>
                   <span class="text-gray-800">{{ locations[product.id] || 'Ubicación no disponible' }}</span>
+                  <span class="mx-2 text-gray-800">•</span>
                   <span class="text-gray-800">{{ timeAgo(product.created_at) }}</span>
                 </div>
 
