bf3a8b4db782ba68ab249ea3b3d093a1b5c0213d
arnaugt25 <agarcia3@cendrassos.net>
2025-02-12 15:19:15 +0100

    listas de productos y eventos y modificacion vista

--- resources/js/Pages/ProducteAmpliat.vue
+++ resources/js/Pages/ProducteAmpliat.vue
@@ -301,6 +301,9 @@ const isBasket = (product) => {
     </div>
     </div>
   </component>
+  <div v-if="isEditEventModalOpen" class="modal">
+    <!-- Contenido del modal -->
+  </div>
 </template>
 
 <style scoped>
