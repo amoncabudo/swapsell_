eb06e2622f93db7325fd093c17835e3509b711f7
amoncayo <amoncayo@cendrassos.net>
2025-02-13 18:29:45 +0100

    Implementació microsoft clarity

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,6 +19,14 @@
         <script>
             window.__WS_TOKEN__ = '{{ csrf_token() }}';
         </script>
+
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "q9ajw3njsx");
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
