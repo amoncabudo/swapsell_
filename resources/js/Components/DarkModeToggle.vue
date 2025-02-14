7aa4651c3d5ddef063272fa14a94b5ee01d92d03
Jho077 <jespinoza@cendrassos.net>
2025-02-14 16:37:23 +0100

    #267 implementacion del dark mode en navbar y welcome

---
+++ resources/js/Components/DarkModeToggle.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const isDark = ref(localStorage.getItem('theme') === 'dark');
+
+// Aplicar el modo oscuro según la preferencia guardada
+onMounted(() => {
+  if (isDark.value) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+});
+
+// Alternar el modo oscuro
+const toggleDarkMode = () => {
+  isDark.value = !isDark.value;
+  if (isDark.value) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  }
+};
+</script>
+
+<template>
+  <button @click="toggleDarkMode" class="p-2 text-black dark:text-white">
+    <span  v-if="isDark">☀️</span>
+    <span v-else>🌙</span>
+</button>
+</template>
