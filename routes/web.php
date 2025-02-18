b8e4f4d09313292e9bf69e85389c79f9906652c7
arnaugt25 <agarcia3@cendrassos.net>
2025-02-18 15:56:24 +0100

    server prova

--- routes/web.php
+++ routes/web.php
@@ -101,7 +101,9 @@ Route::get('/updateEvent/{id}', [EventController::class, "goEvent"])->middleware
 Route::post('/updateEvent', [EventController::class, "updateEvent"])->middleware(['auth', 'verified'])->name('updateEvent');
 
 //Route DeleteEvent
-Route::post('/deleteEvent/{id}', [EventController::class, "deleteEvent"])->middleware(ManagerMiddleware::class)->name("deleteEvent");
+Route::get('/deleteEvent/{id}', [EventController::class, "deleteEvent"])->middleware(ManagerMiddleware::class)->name("deleteEvent");
+
+Route::post('/deleteEvent', [EventController::class, "deleteEvent"])->middleware(ManagerMiddleware::class)->name("deleteEvent");
 
 //Route EventInfo
 Route::get('/eventInfo', function(){return Inertia::render('EventInfo',['isAuthenticated' => auth()->check(),]);});
