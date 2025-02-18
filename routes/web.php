d681771d2c13eff253283a5bba77bb8c97b6d5d8
arnaugt25 <agarcia3@cendrassos.net>
2025-02-18 16:04:19 +0100

    server prova

--- routes/web.php
+++ routes/web.php
@@ -123,8 +123,7 @@ Route::post('/products', [ProductController::class, "addProduct"])->name("produc
 
 //Route UpdateProduct
 Route::get('/updateProduct', function(){return Inertia::render('UpdateProduct',['isAuthenticated' => auth()->check(),]);})->middleware(['auth', 'verified'])->name('updateProduct');;
-Route::get('/updateProduct/{id}', [ProductController::class, "updateProduct"])->middleware(['auth', 'verified'])->name("updateProductId");
-Route::post('/updateProduct/{id}', [ProductController::class, "updateProduct"])->name("updateProduct");
+Route::put('/updateProduct/{id}', [ProductController::class, "updateProduct"])->middleware(['auth', 'verified'])->name("updateProductId");
 Route::get('/editProduct/{id}', [ProductController::class, "editProduct"])->name("editProductId");
 
 //Route DeleteProduct
